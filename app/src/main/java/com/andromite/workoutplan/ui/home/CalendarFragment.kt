6a8c630c36fac4c33e565aebc191e5bf81f8d98a
Mohammed Faizan Hussain <faizan@brochill.com>
2022-09-12 15:18:22 +0530

    [UI] names refactoring

--- app/src/main/java/com/andromite/workoutplan/ui/home/CalendarFragment.kt
+++ app/src/main/java/com/andromite/workoutplan/ui/home/CalendarFragment.kt
@@ -35,7 +35,7 @@ class EditWorkoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_workout, container, false)
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 
     companion object {
